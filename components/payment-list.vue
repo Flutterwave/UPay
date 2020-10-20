8c0121068d1c09a6b99dea4ec12a4a6ef3ac91a4
Toyeeb <toyeeb@flutterwavego.com>
2020-10-20 13:20:51 +0100

    Added Ant Design Library

---
+++ components/payment-list.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <div>
+
+    <div>
+      <p>Welcome John Doe</p>
+
+      <p>Available Balance : #3000</p>
+    </div>
+
+
+    <div>
+
+
+
+      <a-card title="Available Services">
+
+
+      <a-card-grid style="width:50%;text-align:center">
+        <NuxtLink to="/make-payment/:airtime"> Airtime</NuxtLink>
+      </a-card-grid>
+
+      <a-card-grid style="width:50%;text-align:center">
+        <NuxtLink to="/make-payment/data"> Data</NuxtLink>
+
+      </a-card-grid>
+
+
+      <a-card-grid style="width:50%;text-align:center">
+        <NuxtLink to="/make-payment/cable"> Cable</NuxtLink>
+
+      </a-card-grid>
+
+      <a-card-grid style="width:50%;text-align:center">
+        <NuxtLink to="/make-payment/power"> Power</NuxtLink>
+
+      </a-card-grid>
+
+      </a-card>
+    </div>
+
+
+
+
+
+
+    <div>
+
+
+
+    </div>
+
+
+  </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: "PaymentList"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,92 @@
+<template>
+
+  <div>
+
+    <a-tabs tab-position="top">
+      <a-tab-pane key="fund">
+        <template v-slot:tab>
+        <span>
+Fund Wallet        </span>
+        </template>
+
+        <fund-wallet></fund-wallet>
+
+      </a-tab-pane>
+      <a-tab-pane key="payment">
+        <template v-slot:tab>
+        <span>
+          Make Payment
+        </span>
+        </template>
+       <payment-list></payment-list>
+      </a-tab-pane>
+
+      <a-tab-pane key="history">
+        <template v-slot:tab>
+        <span>
+          History
+        </span>
+        </template>
+<history></history>
+      </a-tab-pane>
+
+
+
+
+
+
+    </a-tabs>
+
+  </div>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+
+
+export default Vue.extend({
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
