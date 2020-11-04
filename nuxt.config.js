7949c97ee184cbef1f0502cd56ac1bc22bbf99d0
Toyeeb <toyeeb@flutterwavego.com>
2020-11-04 11:47:11 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'demo',
+    title: 'UPay',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
